2d46dd8a181562d75db34e00f092a645d3c13ba1
mayank joshi <mayankjsohi1910@gmail.com>
2022-07-22 12:41:29 +0530

    git push

---
+++ test1.py
@@ -0,0 +1,15 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://mayank:mayank@cluster0.trwkkyl.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+d = {
+    "name":"mayank",
+    "email":"mayank@gmail.com",
+    "surname":"joshi"
+}
+
+db1=client['mongotest']
+coll=db1['test']
+coll.insert_one(d)
